a430c95194e637c4f5b11d5393f7bd90ed878787
toxi42 <79251578+toxi42@users.noreply.github.com>
2021-12-23 13:20:27 +0300

    Create kaspa-miner-install.sh

---
+++ kaspa-miner-install.sh
@@ -0,0 +1,30 @@
+apt update
+apt install libc6 -y
+logs-off
+mkdir /miner
+wget -P /miner https://github.com/elichai/kaspa-miner/releases/download/v0.1.3/kaspa-miner-v0.1.3-linux-amd64
+chmod +x /miner/kaspa-miner-v0.1.3-linux-amd64
+
+sudo tee /etc/systemd/system/miner444.service > /dev/null <<EOF
+[Unit]
+Description=kaspa miner
+After=rc-local.service
+
+[Service]
+RestartSec=5
+Restart=always
+#RuntimeMaxSec=3600
+WorkingDirectory=/miner
+TimeoutSec=180
+ExecStart=/miner/kaspa-miner-v0.1.3-linux-amd64 --kaspad-address NODE_IP_ADDR --mining-address kaspa:ADDRESS_FROM_NODE
+
+[Install]
+WantedBy=multi-user.target
+Alias=miner444.service
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable miner444
+sudo systemctl restart miner444
+
+journalctl -f -u miner444
