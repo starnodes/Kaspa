90484fc95e5d3d70569b161beee771c59b50d68f
toxi42 <79251578+toxi42@users.noreply.github.com>
2021-12-26 17:37:55 +0300

    Update kaspa-miner-install.sh

--- kaspa-miner-install.sh
+++ kaspa-miner-install.sh
@@ -1,9 +1,8 @@
 apt update
 apt install libc6 -y
-logs-off
 mkdir /miner
-wget -P /miner https://github.com/elichai/kaspa-miner/releases/download/v0.1.3/kaspa-miner-v0.1.3-linux-amd64
-chmod +x /miner/kaspa-miner-v0.1.3-linux-amd64
+wget -P /miner https://github.com/elichai/kaspa-miner/releases/download/v0.2.0/kaspa-miner-v0.2.0-linux-amd64
+chmod +x /miner/kaspa-miner-v0.2.0-linux-amd64
 
 sudo tee /etc/systemd/system/miner444.service > /dev/null <<EOF
 [Unit]
@@ -16,7 +15,7 @@ Restart=always
 #RuntimeMaxSec=3600
 WorkingDirectory=/miner
 TimeoutSec=180
-ExecStart=/miner/kaspa-miner-v0.1.3-linux-amd64 --kaspad-address NODE_IP_ADDR --mining-address kaspa:ADDRESS_FROM_NODE
+ExecStart=/miner/kaspa-miner-v0.2.0-linux-amd64 --kaspad-address NODE_IP_ADDR --mining-address kaspa:ADDRESS_FROM_NODE
 
 [Install]
 WantedBy=multi-user.target
